9799fa62ede85f350a8623159d77ce030b5dfc7b
Csilla Varga <vargacsillamagda@gmail.com>
2017-11-30 15:27:12 +0100

    feat(Praise): create Praise class

---
+++ GitProWorkshop/Models/Praise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProWorkshop.Models
+{
+    public class Praise
+    {
+        string firstAdjective;
+        string secondAdjective;
+        int multiply;
+    }
+}
