58805de74becad338b5bb3a85bf0f33b12a1a773
Csilla Varga <vargacsillamagda@gmail.com>
2017-12-01 13:53:47 +0100

    feat(GitProWorkShop): add database packages

---
+++ GitProWorkshop/Entities/WorkshopContext.cs
@@ -0,0 +1,18 @@
+﻿using GitProWorkshop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProWorkshop.Entities
+{
+    public class WorkshopContext : DbContext
+    {
+        public DbSet<Praise> Praises;
+
+        public WorkshopContext(DbContextOptions<WorkshopContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ GitProWorkshop/Repositories/WorkshopRepository.cs
@@ -0,0 +1,18 @@
+﻿using GitProWorkshop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProWorkshop.Repositories
+{
+    public class WorkshopRepository
+    {
+        WorkshopContext WorkshopContext;
+
+        public WorkshopRepository(WorkshopContext workshopContext)
+        {
+            WorkshopContext = workshopContext;
+        }
+    }
+}
